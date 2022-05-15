cb6da6631604fb23930a9ee8474ace60c62b0437
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-15 23:04:53 +0100

    added fourth problem statement

---
+++ frequency_counter/sameFrequency.js
@@ -0,0 +1,13 @@
+/**
+ * Given two positive integers, find out if the two numbers have the same
+ * frequency of digits.
+ * 
+ * Must have linear time complexity -- O(n)
+ */
+
+function sameFrequency(num1, num2) {}
+
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3_589_578, 5_879_385)); // true
+console.log(sameFrequency(22, 222)); // false
