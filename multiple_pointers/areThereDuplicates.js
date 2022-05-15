ddcf8c06eb5c73d9faa6af3e67a3a657b3bfa7d5
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-15 23:18:00 +0100

    added fifth problem statement

---
+++ multiple_pointers/areThereDuplicates.js
@@ -0,0 +1,16 @@
+/**
+ * Note: This can be solved using either the multiple pointers or frequency
+ * counter method.
+ * 
+ * Implement a function which accepts a variable number of arguments and checks
+ * whether there are any duplicates among the arguments passed in.
+ * 
+ * Must have linear time and space complexity -- O(n)
+ * Bonus: Time -- O(n log n), Space -- O(1)
+ */
+
+function areThereDuplicates() {}
+
+console.log(areThereDuplicates(1, 2, 3)); // false
+console.log(areThereDuplicates(1, 2, 2)); // true
+console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
