81cea9e9a37c6955b66a2da9a60996de86c5f19e
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-17 18:08:40 +0100

    added sixth problem

---
+++ multiple_pointers/averagePair.js
@@ -0,0 +1,16 @@
+/**
+ * Multiple Pointers - Problem 6
+ * -----------------------------
+ * Given a sorted array of integers and a target average, determine if there is
+ * a pair of values in the array where the average pair equals the target
+ * average. There may be more than one pair that matches the average target.
+ * 
+ * Must have linear time complexity O(n) and constant space complexity O(1).
+ */
+
+function averagePair(ascendingNumbers, targetAvg) {}
+
+console.log(averagePair([1, 2, 3], 2.5));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+console.log(averagePair([], 4));
