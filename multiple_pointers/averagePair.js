e5930eb6d37f403cacb8735c7631be33f13f5865
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-18 20:00:15 +0100

    added written solution for problem six

--- multiple_pointers/averagePair.js
+++ multiple_pointers/averagePair.js
@@ -8,7 +8,21 @@
  * Must have linear time complexity O(n) and constant space complexity O(1).
  */
 
-function averagePair(ascendingNumbers, targetAvg) {}
+function averagePair(ascendingNumbers, targetAvg) {
+  /**
+   * 1. Check array for value matching targetAvg
+   * 2. Compare this value to its immediate neighbour
+   * 3. If same value, done -> return true
+   *      e.g. [...8, 8...] -> (8+8)/2 = 8
+   * 4. Start loop at array values that are just below and just above the
+   *      targetAvg
+   * 5. Find average of current pair
+   * 6. If lower than target, use next highest value
+   * 7. If higher than target, use next lowest value
+   * 8. Once real average matches target, done -> return true
+   * 9. Otherwise, no matching average -> return false
+   */
+}
 
 console.log(averagePair([1, 2, 3], 2.5)); // true
 console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
