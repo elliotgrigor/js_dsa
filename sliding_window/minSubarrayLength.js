b193574a317396e4c16a431e7af423ae4baa7000
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-24 15:52:43 +0100

    added written solution for problem nine

--- sliding_window/minSubarrayLength.js
+++ sliding_window/minSubarrayLength.js
@@ -2,19 +2,28 @@
  * Sliding Window - Problem 9
  * --------------------------
  * Write a function which accepts two parameters - an array of positive
- * integers, and a positive integer - and returns the minimal length of a
- * contiguous subarray of which the sum is greater than or equal to the integer
- * passed to the function. If not, return 0 instead.
+ * integers, and a positive integer - and returns the minimal length of
+ * a contiguous subarray of which the sum is greater than or equal to
+ * the integer passed to the function. If not, return 0 instead.
  *   e.g. [2, 3, 1, 2, 4, 3], 7 -> [2, 3, 1, 2] = 8 (length: 4)
  *                              -> [3, 1, 2, 4] = 10 (length: 4)
  *                              -> [1, 2, 4] = 7 (length: 3)
  *                              -> [2, 4, 3] = 9 (length: 3)
- *                              -> [4, 3] = 7 (length: 2) == shortest, so return
+ *                              -> [4, 3] = 7 (length: 2) == shortest
  * 
- * Must have linear time complexity O(n) and constant space complexity O(1)
+ * Must have linear time complexity O(n) and constant space complexity
+ * O(1)
  */
 
-function minSubarrayLength(nums, target) {}
+function minSubarrayLength(nums, target) {
+  /**
+   * 1. Loop over array, setting window start and end to index 1
+   * 2. If total < target, increase window and add new value
+   * 3. If total >= target, set window length if it's smallest &
+   *      remove first value from the current window
+   * 4. If the the end of the window exceeds the array size, stop
+   */
+}
 
 console.log(minSubarrayLength([2, 3, 1, 2, 4, 3], 7)); // 2, because [4,3]
 console.log(minSubarrayLength([2, 1, 6, 5, 4], 9)); // 2, because [5,4]
