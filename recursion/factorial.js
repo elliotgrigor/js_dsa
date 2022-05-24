b25ecc72bb83de9d8205e9e9207a051011507748
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-24 19:53:04 +0100

    added recursion problem twelve

---
+++ recursion/factorial.js
@@ -0,0 +1,19 @@
+/**
+ * Recursion - Problem 12
+ * ----------------------
+ * Write a function which accepts a number and returns the factorial of
+ * that number.
+ * 
+ * n.b. A factorial is the product of an integer and all the integers
+ * below it.
+ *   e.g. factorial 4 (4!) = 24, because 4 * 3 * 2 * 1
+ * 
+ * Factorial 0 (0!) is always 1.
+ */
+
+function factorial(num) {}
+
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
