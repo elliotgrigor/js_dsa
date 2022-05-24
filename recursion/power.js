cfb3c79587ad701d51213e273ff8ebac50ce40b0
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-24 19:10:09 +0100

    added recursion problem eleven

---
+++ recursion/power.js
@@ -0,0 +1,17 @@
+/**
+ * Recursion - Problem 11
+ * ----------------------
+ * Write a function which accepts a base an exponent. The function
+ * should return the power of the base to the exponent. It should mimic
+ * the functionality of Math.pow()
+ * 
+ * Negative numbers can be ignored.
+ */
+
+function power(base, exp) {}
+
+console.log(power(2, 0)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(2, 4)); // 16
+console.log(power(5, 3)); // 125
+console.log(power(10, 3)); // 1000
