c53c182229b46a847d1c7c2d0f9494de888aeb91
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-24 19:46:10 +0100

    added solution for problem eleven

--- recursion/power.js
+++ recursion/power.js
@@ -8,7 +8,20 @@
  * Negative numbers can be ignored.
  */
 
-function power(base, exp) {}
+function power(base, exp) {
+  /**
+   * 1. If exponent is zero, return 1
+   * 2. If exponent is 1, return the base
+   * 3. Otherwise, return the result of base * base^exp--
+   * 
+   * n.b. 10^3 == 10^2 * 10 == 10^1 * 10 * 10
+   */
+
+  if (exp < 1) return 1;
+  if (exp === 1) return base;
+
+  return base * power(base, exp -= 1);
+}
 
 console.log(power(2, 0)); // 1
 console.log(power(2, 2)); // 4
