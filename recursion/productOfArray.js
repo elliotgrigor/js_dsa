799a087ddaac6a18f3c9b453f734846aba8fe282
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-25 19:04:06 +0100

    added solution for problem thirteen

--- recursion/productOfArray.js
+++ recursion/productOfArray.js
@@ -5,7 +5,23 @@
  * product of them all.
  */
 
-function productOfArray(numbers) {}
+function productOfArray(numbers) {
+  /**
+   * Need to track current element, so a helper function is required.
+   * 
+   * Exit condition is the last index of the array, so
+   * 1. Track current element in array
+   * 2. Use helper function for recursion, using iterator variable to
+   *      check for last index
+   * 3. If last index, return current value alone
+   * 4. Otherwise, multiply current value by next value
+   */
+
+  return (function recur(numbers, i = 0) {
+    if (i >= numbers.length - 1) return numbers[i];
+    return numbers[i] * recur(numbers, i + 1);
+  })(numbers);
+}
 
 console.log(productOfArray([6, 3, 10, 11, 9, 16])); // 285,120
 console.log(productOfArray([-1, 9, 12, 6, -7])); // 4,536
