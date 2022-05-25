e982906afbf3c31209692668e813ab9963349e4a
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-25 19:17:40 +0100

    added problem fourteen

---
+++ recursion/recursiveRange.js
@@ -0,0 +1,13 @@
+/**
+ * Recursion - Problem 14
+ * ----------------------
+ * Write a function which accepts a number and add up all the numbers
+ * from 0 to the number passed into the function.
+ */
+
+function recursiveRange(num) {}
+
+console.log(recursiveRange(9)); // 45
+console.log(recursiveRange(21)); // 231
+console.log(recursiveRange(198)); // 19,701
+console.log(recursiveRange(2033)); // 2,067,561
