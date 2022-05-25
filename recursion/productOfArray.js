619b1f625840b68411b48ea2b2a0f7833ecae6e5
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-25 18:39:12 +0100

    added problem thirteen

---
+++ recursion/productOfArray.js
@@ -0,0 +1,12 @@
+/**
+ * Recursion - Problem 13
+ * ----------------------
+ * Write a function which takes an array of numbers and returns the
+ * product of them all.
+ */
+
+function productOfArray(numbers) {}
+
+console.log(productOfArray([6, 3, 10, 11, 9, 16])); // 55
+console.log(productOfArray([-1, 9, 12, 6, -7])); // 19
+console.log(productOfArray([-3, -10, 9, 3, -8])); // -9
