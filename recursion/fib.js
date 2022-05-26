6aa63ec35118bda1c0e97b5fdb04a623026652b6
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-26 19:21:56 +0100

    added solution for problem 15

--- recursion/fib.js
+++ recursion/fib.js
@@ -5,7 +5,30 @@
  * number in the Fibonacci sequence.
  */
 
-function fib(n) {}
+function fib(n) {
+  /**
+   * 1. Track iteration, current & previous numbers
+   * 2. Save current number before adding previous number onto it
+   * 3. Overwrite previous number with saved "current" number
+   * 4. Recur with n, new numbers, and iteration +1
+   * 5. When iteration matches n, return current number
+   */
+
+  let numbers = { current: 1, previous: null };
+
+  function recur(n, numbers, i = 1) {
+    if (i === n) return numbers.current;
+
+    const temp = numbers.current;
+
+    numbers.current += numbers.previous || 0;
+    numbers.previous = temp;
+
+    return recur(n, numbers, i + 1);
+  }
+
+  return recur(n, numbers);
+}
 
 console.log(fib(4)); // 3
 console.log(fib(10)); // 55
