c7d7491d95c854f58d6d364e5f210273e7308bff
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-26 19:21:18 +0100

    added problem 15

---
+++ recursion/fib.js
@@ -0,0 +1,13 @@
+/**
+ * Recursion - Problem 15
+ * ----------------------
+ * Write a recursive function which accepts a number and returns the nth
+ * number in the Fibonacci sequence.
+ */
+
+function fib(n) {}
+
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317,811
+console.log(fib(35)); // 9,227,465
