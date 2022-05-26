80111d24b3ee05b276087d8acdf16b4069d19af9
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-26 23:37:06 +0100

    added binary search problem

---
+++ divide_and_conquer/binarySearch.js
@@ -0,0 +1,28 @@
+/**
+ * Divide & Conquer - Binary Search
+ * -----------------------------
+ * Write a function which accepts a sorted array and a value, and
+ * returns the index at which the value is stored. Otherwise, return -1.
+ */
+
+function binarySearch(sortedArray, searchTerm) {}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(binarySearch(
+  [5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+   40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+  10,
+)); // 2
+console.log(binarySearch(
+  [5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+   40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+  95,
+)); // 16
+console.log(binarySearch(
+  [5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+   40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+  100,
+)); // -1
