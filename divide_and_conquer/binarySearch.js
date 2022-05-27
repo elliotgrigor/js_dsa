cabfc39a6df2acc0c5b289b73abc8a6161396fe2
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-27 17:52:58 +0100

    added solution for binary search problem

--- divide_and_conquer/binarySearch.js
+++ divide_and_conquer/binarySearch.js
@@ -5,7 +5,34 @@
  * returns the index at which the value is stored. Otherwise, return -1.
  */
 
-function binarySearch(sortedArray, searchTerm) {}
+function binarySearch(sortedArray, searchTerm) {
+  /**
+   * 1. Get middle value of current window
+   * 2. If middle value == searched value, return current index
+   * 3. If less than searched value, shrink window from left
+   * 4. If greater than searched value, shrink window from right
+   * 5. If result not set, return -1
+   */
+
+  let [lPtr, rPtr] = [0, sortedArray.length - 1];
+  let result = -1;
+
+  while (lPtr <= rPtr) {
+    const mid = rPtr - Math.floor((rPtr - lPtr) / 2);
+    
+    if (sortedArray[mid] === searchTerm) {
+      result = mid;
+      break;
+    }
+
+    /* ignore values to left of middle */
+    if (sortedArray[mid] < searchTerm) lPtr = mid + 1;
+    /* ignore values to right of middle */
+    if (sortedArray[mid] > searchTerm) rPtr = mid - 1;
+  }
+
+  return result;
+}
 
 console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
 console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
