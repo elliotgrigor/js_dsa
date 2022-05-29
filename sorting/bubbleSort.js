d21c0248697cb868d3c73e08fa154786cfefc57d
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-29 21:43:55 +0100

    added bubble sort problem

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,12 @@
+/**
+ * Sorting - Bubble Sort
+ * ---------------------
+ * Write a function which accepts an array of randomly arranged integers
+ * and returns the sorted equivalent (ascending).
+ */
+
+function bubbleSort(arr) {}
+
+console.log(bubbleSort([])); // []
+console.log(bubbleSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
+console.log(bubbleSort([19, 12, 3, -1, 6])); // [-1, 3, 6, 12, 19]
