733fbf8f66b4c16039f07733a7515d23f7b24452
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-29 22:08:29 +0100

    added bubble sort solution

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -5,7 +5,24 @@
  * and returns the sorted equivalent (ascending).
  */
 
-function bubbleSort(arr) {}
+function bubbleSort(arr) {
+  let last = arr.length - 1;
+
+  while (last > 0) {
+    /* push next largest number to end */
+    for (let i = 0; i <= last; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+      }
+    }
+    /* set new end */
+    last--;
+  }
+
+  return arr;
+}
 
 console.log(bubbleSort([])); // []
 console.log(bubbleSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
