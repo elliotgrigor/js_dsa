1aee0629409afa29b1ae16d7b082547ce061bb91
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-30 19:51:48 +0100

    added solution for selection sort

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -5,7 +5,30 @@
  * and returns the sorted equivalent (ascending).
  */
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  let start = 0;
+
+  while (start < arr.length - 1) {
+    /* indices, not values */
+    const next = start + 1;
+    let min = start;
+    let minSet = false;
+
+    for (let i = next; i < arr.length; i++) {
+      if (arr[min] > arr[i]) {
+        min = i;
+        minSet = true;
+      }
+    }
+
+    /* only switch if min has been changed */
+    if (minSet) [arr[start], arr[min]] = [arr[min], arr[start]];
+
+    start++;
+  }
+
+  return arr;
+}
 
 console.log(selectionSort([])); // []
 console.log(selectionSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
