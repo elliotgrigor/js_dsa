43262bc232ce6dabe6246fd8c7485f7fb92c7c33
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-30 20:11:23 +0100

    added insertion sort problem

---
+++ sorting/insertionSort.js
@@ -0,0 +1,12 @@
+/**
+ * Sorting - Bubble Sort
+ * ---------------------
+ * Write a function which accepts an array of randomly arranged integers
+ * and returns the sorted equivalent (ascending).
+ */
+
+function insertionSort(arr) {}
+
+console.log(insertionSort([])); // []
+console.log(insertionSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
+console.log(insertionSort([19, 12, 3, -1, 6])); // [-1, 3, 6, 12, 19]
