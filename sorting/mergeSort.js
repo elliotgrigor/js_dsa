d67bc7f52ed8f166e68f51539662a4338db1e106
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-31 17:37:50 +0100

    added merge sort problem

---
+++ sorting/mergeSort.js
@@ -0,0 +1,18 @@
+/**
+ * Sorting - Merge Sort
+ * --------------------
+ * Write a function which accepts an array of randomly arranged integers
+ * and returns the sorted equivalent (ascending).
+ */
+
+/* Problem A - Implement (sorted) array merger */
+function merge(arr1, arr2) {}
+
+console.log(merge([1, 5, 8], [-1, 3, 10, 13])); // [-1, 1, 3, 5, 8, 10, 13]
+
+/* Problem B - Implement sorting algorithm */
+function mergeSort(arr) {}
+
+console.log(mergeSort([])); // []
+console.log(mergeSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
+console.log(mergeSort([19, 12, 3, -1, 6])); // [-1, 3, 6, 12, 19]
