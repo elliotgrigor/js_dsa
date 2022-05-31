78c67e6df427ab5d22692bf446a2af3601c140eb
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-31 16:37:20 +0100

    added solution for insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -5,7 +5,38 @@
  * and returns the sorted equivalent (ascending).
  */
 
-function insertionSort(arr) {}
+function insertionSort(arr) {
+  let start = 1;
+
+  while (start < arr.length) {
+    let static = arr[start];
+
+    for (let i = start - 1; i >= 0; i--) {
+      const [current, next] = [arr[i], arr[i + 1]];
+
+      if (static < current) {
+        /* move bigger value right */
+        arr[i + 1] = current;
+        /* on last iteration, if lowest, set static to first value */
+        if (i === 0) arr[i] = static;
+      }
+      else {
+        if (static <= next) {
+          /**
+           * when static falls between current and next,
+           * set next to static, then stop
+           */
+          arr[i + 1] = static;
+          break;
+        }
+      }
+    }
+
+    start++;
+  }
+
+  return arr;
+}
 
 console.log(insertionSort([])); // []
 console.log(insertionSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
