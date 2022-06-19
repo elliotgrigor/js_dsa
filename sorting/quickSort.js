dfb894d63ae5c4a4cc0f614027b9bf32f54da149
elliotgrigor <elliot.grigor@protonmail.com>
2022-06-19 10:51:47 +0100

    added quick sort problem

---
+++ sorting/quickSort.js
@@ -0,0 +1,16 @@
+/**
+ * Sorting - Quick Sort
+ * --------------------
+ * Write a function which accepts an array of randomly arranged integers
+ * and returns the sorted equivalent (ascending).
+ */
+
+/* Problem A - Implement the pivot point selector */
+function pivot(arr, start = 0, end = arr.length - 1) {}
+
+/* Problem B - Implement the quick sort algorithm */
+function quickSort(arr) {}
+
+console.log(mergeSort([])); // []
+console.log(mergeSort([4, 9, 16, 5, 1])); // [1, 4, 5, 9, 16]
+console.log(mergeSort([19, 12, 3, -1, 6])); // [-1, 3, 6, 12, 19]
